10f4a4f5546929de3c49bcf3a1e701c1f8fe7621
Onyemaechi Okafor <peerwaya@yahoo.com>
2021-10-10 08:31:38 +0100

    refactor

--- lib/index.dart
+++
@@ -1,5 +0,0 @@
-export 'video_preview.dart'; //if (dart.library.html) 'web.dart';
-export 'interface.dart';
-export 'video_controls.dart';
-export 'video_player_focus.dart';
-export 'video_overlay.dart';
