2f0a560d28087469c68f7050f2f8ca2dea37e7f6
Onyemaechi Okafor <onyemaechiokafor@onyemaechis-mbp.lan>
2023-11-10 15:30:04 +0100

    more blurhash

--- lib/preview.dart
+++ lib/preview.dart
@@ -5,6 +5,8 @@ import 'package:browser_adapter/browser_adapter.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:blurhash_ffi/blurhash_ffi.dart';
+import 'package:flutter_blurhash/flutter_blurhash.dart';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:pointer_interceptor/pointer_interceptor.dart';
 import 'package:video_player/video_player.dart';
@@ -39,31 +41,39 @@ class ImageBackdrop extends StatelessWidget {
               imageUrl: imageUrl!,
               fit: BoxFit.cover,
             )
+          : kIsWeb
+              ? BlurHash(
+                  color: blurColor,
+                  hash: blurHash!,
+                  imageFit: BoxFit.cover,
+                  //duration: Duration.zero,
+                  image: imageUrl,
+                )
+              : BlurhashFfi(
+                  color: blurColor,
+                  hash: blurHash!,
+                  imageFit: BoxFit.cover,
+                  //duration: Duration.zero,
+                  image: imageUrl,
+                );
+    }
+
+    if (blurHash != null) {
+      return kIsWeb
+          ? BlurHash(
+              color: blurColor,
+              hash: blurHash!,
+              imageFit: BoxFit.cover,
+              //duration: Duration.zero,
+              image: imageUrl,
+            )
           : BlurhashFfi(
               color: blurColor,
               hash: blurHash!,
               imageFit: BoxFit.cover,
-              duration: Duration.zero,
+              //duration: Duration.zero,
+              image: imageUrl,
             );
-    }
-
-    if (blurHash != null) {
-      return Stack(
-        fit: StackFit.expand,
-        children: [
-          BlurhashFfi(
-            color: blurColor,
-            hash: blurHash!,
-            imageFit: BoxFit.cover,
-            duration: Duration.zero,
-          ),
-          if (imageUrl != null)
-            Image(
-              image: CachedNetworkImageProvider(imageUrl!),
-              fit: BoxFit.contain,
-            )
-        ],
-      );
     }
     if (imageUrl != null && isSafariBrowser()) {
       return Stack(
@@ -91,40 +101,39 @@ class ImageBackdrop extends StatelessWidget {
         ],
       );
     }
-    return Stack(
-      fit: StackFit.expand,
-      children: [
-        blurHash != null
-            ? BlurhashFfi(
+    return blurHash != null
+        ? kIsWeb
+            ? BlurHash(
                 color: blurColor,
                 hash: blurHash!,
                 imageFit: BoxFit.cover,
                 duration: Duration.zero,
+                image: imageUrl,
               )
-            : imageUrl != null
-                ? ClipRect(
-                    child: ImageFiltered(
-                      imageFilter: ImageFilter.blur(sigmaX: 25, sigmaY: 25),
-                      child: Opacity(
-                        opacity: 0.4,
-                        child: Transform.scale(
-                          scale: 3,
-                          child: Image(
-                            image: CachedNetworkImageProvider(imageUrl!),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
+            : BlurhashFfi(
+                color: blurColor,
+                hash: blurHash!,
+                imageFit: BoxFit.cover,
+                duration: Duration.zero,
+                image: imageUrl,
+              )
+        : imageUrl != null
+            ? ClipRect(
+                child: ImageFiltered(
+                  imageFilter: ImageFilter.blur(sigmaX: 25, sigmaY: 25),
+                  child: Opacity(
+                    opacity: 0.4,
+                    child: Transform.scale(
+                      scale: 3,
+                      child: Image(
+                        image: CachedNetworkImageProvider(imageUrl!),
+                        fit: BoxFit.cover,
                       ),
                     ),
-                  )
-                : const SizedBox.shrink(),
-        if (imageUrl != null)
-          Image(
-            image: CachedNetworkImageProvider(imageUrl!),
-            fit: BoxFit.contain,
-          )
-      ],
-    );
+                  ),
+                ),
+              )
+            : const SizedBox.shrink();
   }
 }
 
